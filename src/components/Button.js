334497dd0a8192e136137e82762377aabb52e87c
Romie Bett <bett@Betts-MacBook-Air.local>
2024-07-19 23:08:20 +0300

    Add api to the user view

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import styles from './button.module.css';
 
-const Button = ()=>{
+const Button = (props)=>{
     return (
-       <button className={styles.jokeButton}>
-        Tell me a joke
+       <button onClick={props.onClick} className={styles.jokeButton}>
+       {props.loading ? <>loading...</> : <>Tell Me a Joke</>}
        </button>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,33 @@
+import { useState } from "react";
+import axios from "axios";
 import Button from "./Button";
 import styles from './home.module.css'
 import AnimatedGIF from "./AnimatedGIF";
 import Header from "./Header";
+import Jokes from "./jokes";
 
-const Home = ()=>{
+
+
+const Home = () => {
+    const [joke, setJoke] = useState('');
+    const[loading, setLoading] = useState(false);
+
+    const fetchJoke = () => {
+        setLoading(true)
+        axios.get('https://official-joke-api.appspot.com/jokes/random').then((response) => {
+            setJoke(response.data)
+        }).catch((error)=>{
+            console.log(error);
+        })
+        setLoading(false);
+    };
+
     return (
         <div className={`${styles.app} ${styles.home}`}>
             <Header />
-            <AnimatedGIF link="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExc3pja28waHZoaDN2MnJzNzBwamdnMnUzanduejJndjA0aGZveGJ6YiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xUA7b30EbtkaMHvRgk/giphy.gif"/>
-            <Button /> 
+            <AnimatedGIF link="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExc3pja28waHZoaDN2MnJzNzBwamdnMnUzanduejJndjA0aGZveGJ6YiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xUA7b30EbtkaMHvRgk/giphy.gif" />
+            <Button onClick={fetchJoke} text={loading} />
+            <Jokes content={joke} />
         </div>
     )
 }
